03422bbed0f4e9d4fb43798d5fffccf5509328c7
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2019-07-29 19:36:08 +0200

    [ADD] Secret animation (#1)
    [ADD] ST project
    [ADD] secret animation

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -3,6 +3,8 @@ const $name = document.getElementById("name");
 const $firstLastName = document.getElementById("firstLastName");
 const $secondLastName = document.getElementById("secondLastName");
 const $socialContainer = document.getElementById("socialContainer");
+const $nameButton = document.getElementById("name-btn");
+const $aliasButton = document.getElementById("alias-btn");
 
 $nameContainer.addEventListener('click', (event) => {
     ga('send', 'event', 'social', 'click', 'linkedin', '0');
@@ -10,8 +12,54 @@ $nameContainer.addEventListener('click', (event) => {
 });
 
 $nameContainer.addEventListener('animationend', (event) => {
-    $name.classList.add('showName');
-    $firstLastName.classList.add('showFirstLastName');
-    $secondLastName.classList.add('showSecondLastName');
+    switch(event.animationName) {
+        case "nameContainerAppear":
+            $name.classList.add('showName');
+            $firstLastName.classList.add('showName');
+            $secondLastName.classList.add('showName');
+            break;
+        case "nameAliasResetMove":
+            $name.classList.remove('resetAlias');
+            $name.classList.add('showName');
+            break;
+        case "firstLastNameAliasResetMove":
+            $firstLastName.classList.remove('resetAlias');
+            $firstLastName.classList.add('showName');
+            break;
+        case "secondLastNameAliasResetMove":
+            $secondLastName.classList.remove('resetAlias');
+            $secondLastName.classList.add('showName');
+            break;
+        default:
+            break;
+    }
+});
+
+$aliasButton.addEventListener('click', (event) => {
+    $aliasButton.classList.add('active');
+    $nameButton.classList.remove('active');
+
+    $name.classList.remove('showName');
+    $name.classList.add('showAlias');
+
+    $firstLastName.classList.remove('showName');
+    $firstLastName.classList.add('showAlias');
+
+    $secondLastName.classList.remove('showName');
+    $secondLastName.classList.add('showAlias');
+});
+
+$nameButton.addEventListener('click', (event) => {
+    $nameButton.classList.add('active');
+    $aliasButton.classList.remove('active');
+
+    $name.classList.remove('showAlias');
+    $name.classList.add('resetAlias');
+
+    $firstLastName.classList.remove('showAlias');
+    $firstLastName.classList.add('resetAlias');
+
+    $secondLastName.classList.remove('showAlias');
+    $secondLastName.classList.add('resetAlias');
 });
 
