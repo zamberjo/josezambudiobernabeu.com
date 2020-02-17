c5f4aa536d5c72e63d60bb2f4fa7bfbd0f67025b
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2020-02-17 17:37:10 +0100

    [WIP]

--- web/src/components/name/Name.js
+++ web/src/components/name/Name.js
@@ -15,12 +15,172 @@ class Name extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scrollOptions: {
+      'scrollOptions': {
         throttle: 100,
         scrollOfffset: 500,
         activeClass: 'active',
       },
+      'activeAction': 'alias',
     };
+
+    this.nameContainerClick = this.nameContainerClick.bind(this);
+    this.nameContainerAnima = this.nameContainerAnima.bind(this);
+    this.buttonNameAnimation = this.buttonNameAnimation.bind(this);
+  }
+
+  /**
+   * Click on name animation.
+   */
+  nameContainerClick() {
+    // ga('send', 'event', 'social', 'click', 'linkedin', '0');
+    window.open('https://www.linkedin.com/in/zamberjo');
+  }
+
+  /**
+   * AnimationEnd name container.
+   * @param {Object} event Event animationend
+   */
+  nameContainerAnima(event) {
+    const flagAlias = this._buttonAliasEl.classList.contains('active');
+    const flagName = this._buttonNameEl.classList.contains('active');
+    const flagSocial = this._buttonSocialEl.classList.contains('active');
+
+    switch (event.animationName) {
+      case 'nameContainerAppear':
+        this._nameEl.classList.add('showName');
+        this._firstLastNameEl.classList.add('showName');
+        this._secondLastNameEl.classList.add('showName');
+        break;
+      case 'nameAliasResetMove':
+        this._nameEl.classList.remove('resetAlias');
+        this._nameEl.classList.remove('showName');
+        this._nameEl.classList.remove('showSocial');
+        this._nameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._nameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._nameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._nameEl.classList.add('showAlias');
+        }
+        break;
+      case 'firstLastNameAliasResetMove':
+        this._firstLastNameEl.classList.remove('resetAlias');
+        this._firstLastNameEl.classList.remove('showName');
+        this._firstLastNameEl.classList.remove('showSocial');
+        this._firstLastNameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._firstLastNameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._firstLastNameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._firstLastNameEl.classList.add('showAlias');
+        }
+        break;
+      case 'secondLastNameAliasResetMove':
+        this._secondLastNameEl.classList.remove('resetAlias');
+        this._secondLastNameEl.classList.remove('showName');
+        this._secondLastNameEl.classList.remove('showSocial');
+        this._secondLastNameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._secondLastNameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._secondLastNameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._secondLastNameEl.classList.add('showAlias');
+        }
+        break;
+      case 'nameSocialResetMove':
+        this._nameEl.classList.remove('resetSocial');
+        this._nameEl.classList.remove('showName');
+        this._nameEl.classList.remove('showSocial');
+        this._nameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._nameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._nameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._nameEl.classList.add('showAlias');
+        }
+        break;
+      case 'firstLastNameSocialResetMove':
+        this._firstLastNameEl.classList.remove('resetSocial');
+        this._firstLastNameEl.classList.remove('showName');
+        this._firstLastNameEl.classList.remove('showSocial');
+        this._firstLastNameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._firstLastNameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._firstLastNameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._firstLastNameEl.classList.add('showAlias');
+        }
+        break;
+      case 'secondLastNameSocialResetMove':
+        this._secondLastNameEl.classList.remove('resetSocial');
+        this._secondLastNameEl.classList.remove('showName');
+        this._secondLastNameEl.classList.remove('showSocial');
+        this._secondLastNameEl.classList.remove('showAlias');
+        if ( flagName ) {
+          this._secondLastNameEl.classList.add('showName');
+        } else if ( flagSocial ) {
+          this._secondLastNameEl.classList.add('showSocial');
+        } else if ( flagAlias ) {
+          this._secondLastNameEl.classList.add('showAlias');
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Actions name animation
+   * @param {Object} event Event click
+   */
+  buttonNameAnimation(event) {
+    if ( ! event.target.classList.contains('active') ) {
+      this._buttonNameEl.classList.remove('active');
+      this._buttonAliasEl.classList.remove('active');
+      this._buttonSocialEl.classList.remove('active');
+      event.target.classList.add('active');
+
+      const flagName = this._nameEl.classList.contains('showName');
+      const flagAlias = this._nameEl.classList.contains('showAlias');
+      const flagSocial = this._nameEl.classList.contains('showSocial');
+      this._nameEl.classList.remove('showName');
+      this._firstLastNameEl.classList.remove('showName');
+      this._secondLastNameEl.classList.remove('showName');
+      if ( flagAlias ) {
+        this._nameEl.classList.add('resetAlias');
+        this._firstLastNameEl.classList.add('resetAlias');
+        this._secondLastNameEl.classList.add('resetAlias');
+      } else if ( flagName ) {
+        const flagButtonAlias = this._buttonAliasEl.classList.contains(
+            'active');
+        const flagButtonName = this._buttonNameEl.classList.contains(
+            'active');
+        const flagButtonSocial = this._buttonSocialEl.classList.contains(
+            'active');
+        if ( flagButtonName ) {
+          this._nameEl.classList.add('showName');
+          this._firstLastNameEl.classList.add('showName');
+          this._secondLastNameEl.classList.add('showName');
+        } else if ( flagButtonSocial ) {
+          this._nameEl.classList.add('showSocial');
+          this._firstLastNameEl.classList.add('showSocial');
+          this._secondLastNameEl.classList.add('showSocial');
+        } else if ( flagButtonAlias ) {
+          this._nameEl.classList.add('showAlias');
+          this._firstLastNameEl.classList.add('showAlias');
+          this._secondLastNameEl.classList.add('showAlias');
+        }
+      } else if ( flagSocial ) {
+        this._nameEl.classList.add('resetSocial');
+        this._firstLastNameEl.classList.add('resetSocial');
+        this._secondLastNameEl.classList.add('resetSocial');
+      }
+    }
   }
 
   /**
@@ -32,159 +192,6 @@ class Name extends Component {
     if (typeof M !== 'undefined') {
       this.scrollInst = M.ScrollSpy.init(this._scrollEl, scrollOptions);
     }
-
-    const $nameContainer = document.getElementById('nameContainer');
-    const $name = document.getElementById('name');
-    const $firstLastName = document.getElementById('firstLastName');
-    const $secondLastName = document.getElementById('secondLastName');
-    const $nameButton = document.getElementById('name-btn');
-    const $aliasButton = document.getElementById('alias-btn');
-    const $socialButton = document.getElementById('social-btn');
-
-    $nameContainer.addEventListener('click', (event) => {
-      // ga('send', 'event', 'social', 'click', 'linkedin', '0');
-      window.open('https://www.linkedin.com/in/zamberjo');
-    });
-
-    $nameContainer.addEventListener('animationend', (event) => {
-      const flagAlias = $aliasButton.classList.contains('active');
-      const flagName = $nameButton.classList.contains('active');
-      const flagSocial = $socialButton.classList.contains('active');
-
-      switch (event.animationName) {
-        case 'nameContainerAppear':
-          $name.classList.add('showName');
-          $firstLastName.classList.add('showName');
-          $secondLastName.classList.add('showName');
-          break;
-        case 'nameAliasResetMove':
-          $name.classList.remove('resetAlias');
-          $name.classList.remove('showName');
-          $name.classList.remove('showSocial');
-          $name.classList.remove('showAlias');
-          if ( flagName ) {
-            $name.classList.add('showName');
-          } else if ( flagSocial ) {
-            $name.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $name.classList.add('showAlias');
-          }
-          break;
-        case 'firstLastNameAliasResetMove':
-          $firstLastName.classList.remove('resetAlias');
-          $firstLastName.classList.remove('showName');
-          $firstLastName.classList.remove('showSocial');
-          $firstLastName.classList.remove('showAlias');
-          if ( flagName ) {
-            $firstLastName.classList.add('showName');
-          } else if ( flagSocial ) {
-            $firstLastName.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $firstLastName.classList.add('showAlias');
-          }
-          break;
-        case 'secondLastNameAliasResetMove':
-          $secondLastName.classList.remove('resetAlias');
-          $secondLastName.classList.remove('showName');
-          $secondLastName.classList.remove('showSocial');
-          $secondLastName.classList.remove('showAlias');
-          if ( flagName ) {
-            $secondLastName.classList.add('showName');
-          } else if ( flagSocial ) {
-            $secondLastName.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $secondLastName.classList.add('showAlias');
-          }
-          break;
-        case 'nameSocialResetMove':
-          $name.classList.remove('resetSocial');
-          $name.classList.remove('showName');
-          $name.classList.remove('showSocial');
-          $name.classList.remove('showAlias');
-          if ( flagName ) {
-            $name.classList.add('showName');
-          } else if ( flagSocial ) {
-            $name.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $name.classList.add('showAlias');
-          }
-          break;
-        case 'firstLastNameSocialResetMove':
-          $firstLastName.classList.remove('resetSocial');
-          $firstLastName.classList.remove('showName');
-          $firstLastName.classList.remove('showSocial');
-          $firstLastName.classList.remove('showAlias');
-          if ( flagName ) {
-            $firstLastName.classList.add('showName');
-          } else if ( flagSocial ) {
-            $firstLastName.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $firstLastName.classList.add('showAlias');
-          }
-          break;
-        case 'secondLastNameSocialResetMove':
-          $secondLastName.classList.remove('resetSocial');
-          $secondLastName.classList.remove('showName');
-          $secondLastName.classList.remove('showSocial');
-          $secondLastName.classList.remove('showAlias');
-          if ( flagName ) {
-            $secondLastName.classList.add('showName');
-          } else if ( flagSocial ) {
-            $secondLastName.classList.add('showSocial');
-          } else if ( flagAlias ) {
-            $secondLastName.classList.add('showAlias');
-          }
-          break;
-        default:
-          break;
-      }
-    });
-
-    const buttonNameAnimation = (event) => {
-      if ( ! event.target.classList.contains('active') ) {
-        $nameButton.classList.remove('active');
-        $socialButton.classList.remove('active');
-        $aliasButton.classList.remove('active');
-        event.target.classList.add('active');
-
-        const flagName = $name.classList.contains('showName');
-        const flagAlias = $name.classList.contains('showAlias');
-        const flagSocial = $name.classList.contains('showSocial');
-        $name.classList.remove('showName');
-        $firstLastName.classList.remove('showName');
-        $secondLastName.classList.remove('showName');
-        if ( flagAlias ) {
-          $name.classList.add('resetAlias');
-          $firstLastName.classList.add('resetAlias');
-          $secondLastName.classList.add('resetAlias');
-        } else if ( flagName ) {
-          const flagButtonAlias = $aliasButton.classList.contains('active');
-          const flagButtonName = $nameButton.classList.contains('active');
-          const flagButtonSocial = $socialButton.classList.contains('active');
-          if ( flagButtonName ) {
-            $name.classList.add('showName');
-            $firstLastName.classList.add('showName');
-            $secondLastName.classList.add('showName');
-          } else if ( flagButtonSocial ) {
-            $name.classList.add('showSocial');
-            $firstLastName.classList.add('showSocial');
-            $secondLastName.classList.add('showSocial');
-          } else if ( flagButtonAlias ) {
-            $name.classList.add('showAlias');
-            $firstLastName.classList.add('showAlias');
-            $secondLastName.classList.add('showAlias');
-          }
-        } else if ( flagSocial ) {
-          $name.classList.add('resetSocial');
-          $firstLastName.classList.add('resetSocial');
-          $secondLastName.classList.add('resetSocial');
-        }
-      }
-    };
-
-    $aliasButton.addEventListener('click', buttonNameAnimation);
-    $nameButton.addEventListener('click', buttonNameAnimation);
-    $socialButton.addEventListener('click', buttonNameAnimation);
   }
 
   /**
@@ -208,20 +215,34 @@ class Name extends Component {
         className='scrollspy Presentation'
         ref={(el) => (this._scrollEl = el)}>
         <div id='switch' className='hide-on-small-only'>
-          <i id='name-btn' className='fas fa-user-tie active'></i>
-          <i id='alias-btn' className='fas fa-user-secret'></i>
-          <i id='social-btn' className='fas fa-user'></i>
+          <i id='name-btn'
+            className='fas fa-user-tie active'
+            ref={(el) => (this._buttonNameEl = el)}
+            onClick={this.buttonNameAnimation}></i>
+          <i id='alias-btn'
+            className='fas fa-user-secret'
+            ref={(el) => (this._buttonAliasEl = el)}
+            onClick={this.buttonNameAnimation}></i>
+          <i id='social-btn'
+            className='fas fa-user'
+            ref={(el) => (this._buttonSocialEl = el)}
+            onClick={this.buttonNameAnimation}></i>
         </div>
-        <div id='nameContainer'>
-          <div id='name'>
+        <div id='nameContainer'
+          onClick={this.nameContainerClick}
+          onAnimationEnd={this.nameContainerAnima}>
+          <div id='name'
+            ref={(el) => (this._nameEl = el)}>
             <span className='capitalize'>J</span>
             <span className='other'>ose</span>
           </div>
-          <div id='firstLastName'>
+          <div id='firstLastName'
+            ref={(el) => (this._firstLastNameEl = el)}>
             <span className='capitalize'>Z</span>
             <span className='other'>ambudio</span>
           </div>
-          <div id='secondLastName'>
+          <div id='secondLastName'
+            ref={(el) => (this._secondLastNameEl = el)}>
             <span className='capitalize'>B</span>
             <span className='other'>ernabeu</span>
           </div>
