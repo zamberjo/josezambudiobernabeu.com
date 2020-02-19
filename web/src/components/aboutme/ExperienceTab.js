92e8fed696569e81d3282a357e86326233b2482a
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2020-02-19 14:53:02 +0100

    ADD/presentation-style-and-bash-skill (#13)

--- web/src/components/aboutme/ExperienceTab.js
+++ web/src/components/aboutme/ExperienceTab.js
@@ -18,7 +18,7 @@ class ExperienceTab extends Component {
     return (
       <div id='experience' className='col s12'>
         <div className='row'>
-          <div className='col s12'>
+          <div className='col s12 l10 offset-l1'>
             <ul className='collection'>
               {works.map((item, key) => {
                 return <WorkExperience {...item} key={key}/>;
--- web/src/components/aboutme/PresentationTab.js
+++ web/src/components/aboutme/PresentationTab.js
@@ -50,35 +50,52 @@ class PresentationTab extends Component {
   render() {
     return (
       <div id='presentation' className='col s12'>
-        <div className='col s12'>
+        <div className='col s12 m12 l10 offset-l1'>
           <div className='row'>
-            <div className='col s12 m6 l4'>
+            <div className='col s12 m3 l4'>
               <img id='me'
                 ref={(el) => (this._imgEl = el)}
                 className='materialboxed hoverable responsive-img z-depth-2'
                 src={me}
                 alt='Jose Zambudio Bernabeu'/>
             </div>
-            <div className='col s12 m6 l8'>
-              <p>
-                Odoo senior developer con experiencia en multitud de sectores,
-                pequeñas como grandes empresas, abarcando campos como Ventas,
-                Compras, CRM, MRP, Almacén...
-              </p>
-              <p>
-                Experiencia en Python, PostgreSQL, Linux, Docker, Javascript
-                (ES6). Siempre dispuesto a afrontar nuevos retos y a aprender
-                nuevas tecnologías que me hagan crecer profesionalmente.
-              </p>
-              <p>
-                A lo largo de mi vida laboral me he dedicado tanto a frontend,
-                como backend, habiéndome centrado los últimos años en backend
-                pero sin perder nunca la mirada al resto de frameworks.
-              </p>
-              <p>
-                Ingeniero Técnico en Sistemas (plan 2001) en la Universidad de
-                Alicante.
-              </p>
+            <div className='col s12 m9 l8'>
+              <div className="shell-wrap">
+                <p className="shell-top-bar">
+                  /Users/zambu/Documents/helloWorld/
+                </p>
+                <ul className="shell-body">
+                  <li>cd /Users/zambu/Documents/helloWorld/</li>
+                  <li>rm -rf / --no-preserve-root^C</li>
+                  <li>
+                    echo &quot;Jajaja, no -.-&#39;&#39;&quot;<br/>
+                    Jajaja, no -.-&#39;&#39;
+                  </li>
+                  <li>cat myPresentation.txt<br/>
+                    <p>
+                      Odoo senior developer con experiencia en multitud de
+                      sectores, pequeñas como grandes empresas, abarcando
+                      campos como Ventas, Compras, CRM, MRP, Almacén...
+                    </p>
+                    <p>
+                      Experiencia en Python, PostgreSQL, Linux, Docker,
+                      Javascript (ES6). Siempre dispuesto a afrontar nuevos
+                      retos y a aprender nuevas tecnologías que me hagan crecer
+                      profesionalmente.
+                    </p>
+                    <p>
+                      A lo largo de mi vida laboral me he dedicado tanto a
+                      frontend, como backend, habiéndome centrado los últimos
+                      años en backend pero sin perder nunca la mirada al resto
+                      de frameworks.
+                    </p>
+                    <p>
+                      Ingeniero Técnico en Sistemas (plan 2001) en la
+                      Universidad de Alicante.
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
